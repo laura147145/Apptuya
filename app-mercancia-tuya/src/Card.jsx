fbc0a22e0dec39c87475a6487ba774e818340399
Maiky Rivera <maikygoezrivera@gmail.com>
2025-03-05 16:08:14 -0500

    se agrego los propts

--- app-mercancia-tuya/src/Card.jsx
+++ app-mercancia-tuya/src/Card.jsx
@@ -1,7 +1,14 @@
-function Card(){
+
+function Card({info}){
+    console.log(info);
+    
     return (
         <section className="panel-control-card">
-                <div></div>
+                <div>
+                    <h2>Id:{info.id}</h2>
+                    <h3>Destino:{info.destino}</h3>
+                    <h3>Origen:{info.origen}</h3>
+                </div>
                 <div>Ver</div>
             </section>
     )
--- app-mercancia-tuya/src/PanelControl.jsx
+++ app-mercancia-tuya/src/PanelControl.jsx
@@ -3,14 +3,34 @@ import Card from './Card';
 import Menulateral from './Menulateral';
 
 const PanelControl = () => {
+    let envios =[
+        {
+            origen:"Medellin",
+            destino: "Bogota",
+            id:"PEDIDO2390"
+        },
+        {
+            origen:"Medellin",
+            destino:"Cali",
+            id: "PEDIDO908"
+        },
+        {
+            origen:"Medellin",
+            destino:"Nekocli",
+            id:"PEDIDO4564"
+        },
+    ]
 
     return (
         <section className="panel-control">
             <Menulateral></Menulateral>
             <div className="panel-control-contenido">
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
+                {
+                    envios.map((envio)=>{
+                        return<Card info={envio} />
+                    }
+                    )
+                }
             </div>
         </section>
     )
